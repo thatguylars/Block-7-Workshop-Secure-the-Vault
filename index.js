4aa26b17abacb1d8f8784bd203fdc6038992fb88
Larry <LDLB9255@GMAIL.COM>
2024-09-26 20:04:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+/* 
+define message
+ */
+const message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+/*
+define the variable
+*/
+let num = 10;
+let numOne = num + 0;
+let numTwo = num * 4;
+let numThree = numTwo - 1;
+/* 
+string numone numtwo numthree
+ */
+const str = `${message} ${numOne} ${numTwo} ${numThree}`;
+/*
+put string in pop up window
+*/
+alert(str);
+/*
+Run string
+*/
+console.log(str);
